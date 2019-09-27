7f043f5c447676c92dc08aafb7b848c1f02bf388
H Goto <gh6141@gmail.com>
2019-09-27 17:03:21 +0900

    genso

---
+++ src/src/Gensi.elm
@@ -0,0 +1,82 @@
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
+--
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+-- Data
+data : List (String,String) 
+data= [("ナトリウム原子","Na"),("水素原子","H"),("酸素原子","O"),("塩素原子","Cl"),("硫黄原子","S"),("鉄原子","Fe"),("アルミニウム原子","Al"),("銅原子","Cu"),("亜鉛原子","Zn"),("炭素原子","C"),("窒素原子","N"),("カリウム原子","K"),("カルシウム原子","Ca"),("バリウム原子","Ba"),("銀原子","Ag")]
+
+--Func
+getAt : Int -> List a -> Maybe a
+getAt idx xs =
+    if idx < 0 then
+        Nothing
+    else
+        List.head <| List.drop idx xs
+
+fst:(String,String) -> String
+fst tpl =
+  let
+    (fstx,_) = tpl
+  in
+    fstx
+
+snd:(String,String) -> String
+snd tpl =
+  let
+    (_,sndx) = tpl
+  in
+    sndx
+
+
+-- MODEL
+
+type MaruBatu = Maru | Batu | Mada 
+
+type alias Model = {num:Int,name:String,siki:String,seikai:MaruBatu}
+
+init : Model
+init = {num=0,name="",siki="",seikai=Mada}
+  
+
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      {model | num=model.num + 1 , name= fst (Maybe.withDefault ("","")  (getAt model.num data)  ) 
+      ,siki=  snd (Maybe.withDefault ("","")  (getAt model.num data)  ) 
+      }
+
+    Decrement ->
+      {model | num=model.num - 1 , name= fst (Maybe.withDefault ("","")  (getAt model.num data)  )
+      , siki= snd (Maybe.withDefault ("","")  (getAt model.num data)  ) 
+      }
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model.num) ]
+    , button [ onClick Increment ] [ text "+" ]
+    , div [Html.Attributes.style "font-size" "70pt"] [text model.name]
+    , div [Html.Attributes.style "font-size" "100pt"] [text model.siki]
+    ]
