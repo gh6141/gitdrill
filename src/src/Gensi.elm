a20c914bdd050642652d1c7634d18ce620a797c3
H Goto <gh6141@gmail.com>
2019-09-30 15:50:23 +0900

    kansei

--- src/src/Gensi.elm
+++ src/src/Gensi.elm
@@ -8,29 +8,38 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
-import Random
+import Random exposing (Seed, int, step,initialSeed)
 
-partThree : List a -> Random.Generator (List a)
-partThree list =
-    let
-        randomNumbers : Random.Generator (List Int)
-        randomNumbers =
-            Random.list (List.length list) <| Random.int Random.minInt Random.maxInt
-
-        zipWithList : List Int -> List ( a, Int )
-        zipWithList intList =
-            List.map2 Tuple.pair list intList
-
-        listWithRandomNumbers : Random.Generator (List ( a, Int ))
-        listWithRandomNumbers =
-            Random.map zipWithList randomNumbers
+shuffleList : Seed -> List a -> List a
+shuffleList seed list =
+    shuffleListHelper seed list []
 
 
-        sortedGenerator : Random.Generator (List ( a, Int ))
-        sortedGenerator =
-            Random.map (List.sortBy Tuple.second) listWithRandomNumbers
-    in
-    Random.map (List.map Tuple.first) sortedGenerator
+shuffleListHelper : Seed -> List a -> List a -> List a
+shuffleListHelper seed source result =
+    if List.isEmpty source then
+        result
+    else
+        let
+            indexGenerator =
+                int 0 ((List.length source) - 1)
+
+            ( index, nextSeed ) =
+                step indexGenerator seed
+
+            valAtIndex =
+                getAt index source
+
+            sourceWithoutIndex =
+                removeAt index source
+        in
+            case valAtIndex of
+                Just val ->
+                    shuffleListHelper nextSeed sourceWithoutIndex (val :: result)
+
+                Nothing ->
+                    []
+
 
 
 main =
@@ -47,6 +56,27 @@ getAt idx xs =
         Nothing
     else
         List.head <| List.drop idx xs
+
+removeAt : Int -> List a -> List a
+removeAt index l =
+    if index < 0 then
+        l
+
+    else
+        let
+            head =
+                List.take index l
+
+            tail =
+                List.drop index l |> List.tail
+        in
+        case tail of
+            Nothing ->
+                l
+
+            Just t ->
+                List.append head t
+
 
 fst:(String,String) -> String
 fst tpl =
@@ -67,10 +97,10 @@ snd tpl =
 
 type MaruBatu = Maru | Batu | Mada 
 
-type alias Model = {num:Int,name:String,siki:String,seikai:MaruBatu,sikil:List String}
+type alias Model = {num:Int,name:String,siki:String,seikai:MaruBatu,sikil:List String, marukei:Int}
 
 init : Model
-init = {num=0,name="",siki="",seikai=Mada,sikil=["","",""]}
+init = {num=0,name="",siki="",seikai=Mada,sikil=["","",""],marukei=0}
   
 
 -- UPDATE
@@ -79,13 +109,16 @@ type Msg = Increment | Decrement | B1 | B2 | B3
 
 update : Msg -> Model -> Model
 update msg model =
+ let
+     yoso idx = snd (Maybe.withDefault ("","")  (getAt idx data))
+ in
+ 
   case msg of
     Increment ->
-      {model | num=model.num + 1 , name= fst (Maybe.withDefault ("","")  (getAt model.num data)  ) 
+      {model | seikai=Mada
+      , num=if model.num==(List.length data) then 0 else model.num+1, name= fst (Maybe.withDefault ("","")  (getAt model.num data)  ) 
       ,siki=  snd (Maybe.withDefault ("","")  (getAt model.num data)  )
-      ,sikil= partThree [snd (Maybe.withDefault ("","")  (getAt 4 data)  ),snd (Maybe.withDefault ("","")  (getAt 5 data)  ),snd (Maybe.withDefault ("","")  (getAt model.num data)  )]
-             
-      }
+      ,sikil= shuffleList (Random.initialSeed model.num) [yoso (Basics.modBy  (List.length data) (model.num + 1) )  ,yoso (Basics.modBy   (List.length data) (model.num+2)) , yoso model.num] }
 
     Decrement ->
       {model | num=model.num - 1 , name= fst (Maybe.withDefault ("","")  (getAt model.num data)  )
@@ -93,13 +126,13 @@ update msg model =
       }
 
     B1 ->
-      {model|seikai=Maru}
+      {model|seikai=if (Maybe.withDefault "" (getAt 0 model.sikil))== model.siki then Maru else Batu,marukei=if (Maybe.withDefault "" (getAt 0 model.sikil))== model.siki  then model.marukei+1 else model.marukei}
     
     B2 ->
-      {model|seikai=Maru}
+      {model|seikai=if (Maybe.withDefault "" (getAt 1 model.sikil))== model.siki then Maru else Batu ,marukei=if (Maybe.withDefault "" (getAt 1 model.sikil))== model.siki  then model.marukei+1 else model.marukei}
     
     B3 ->
-      {model|seikai=Maru}
+      {model|seikai=if (Maybe.withDefault "" (getAt 2 model.sikil))== model.siki then Maru else Batu ,marukei=if (Maybe.withDefault "" (getAt 2 model.sikil))== model.siki  then model.marukei+1 else model.marukei}
 
 
 -- VIEW
@@ -107,12 +140,13 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ button [Html.Attributes.style "font-size" "70pt", onClick Decrement ] [ text "もどる" ]
-    , div [] [ text (String.fromInt model.num) ]
-    , button [ Html.Attributes.style "font-size" "70pt",onClick Increment ] [ text "つぎへ" ]
-    , div [Html.Attributes.style "font-size" "70pt"] [text model.name]
-    , div [Html.Attributes.style "font-size" "100pt"] [text model.siki]
-    , button [ Html.Attributes.style "font-size" "100pt",onClick B1 ] [ text (Maybe.withDefault "" (getAt 0 model.sikil)) ]
-    , button [ Html.Attributes.style "font-size" "100pt",onClick B2 ] [ text (Maybe.withDefault "" (getAt 1 model.sikil)) ]
-    , button [ Html.Attributes.style "font-size" "100pt",onClick B3 ] [ text (Maybe.withDefault "" (getAt 2 model.sikil)) ]
+    [ --button [Html.Attributes.style "font-size" "30pt", onClick Decrement ] [ text "もどる" ],
+     div [] [ text (String.fromInt model.num) ]
+    , button [ Html.Attributes.style "font-size" "30pt",onClick Increment ] [ text "つぎへ" ]
+    , div [Html.Attributes.style "font-size" "60pt"] [text model.name]
+    --, div [Html.Attributes.style "font-size" "80pt"] [text model.siki]
+    , button [ Html.Attributes.style "margin" "5pt",Html.Attributes.style "font-size" "100pt",onClick B1 ] [ text (Maybe.withDefault "" (getAt 0 model.sikil)) ]
+    , button [ Html.Attributes.style "margin" "5pt",Html.Attributes.style "font-size" "100pt",onClick B2 ] [ text (Maybe.withDefault "" (getAt 1 model.sikil)) ]
+    , button [ Html.Attributes.style "margin" "5pt",Html.Attributes.style "font-size" "100pt",onClick B3 ] [ text (Maybe.withDefault "" (getAt 2 model.sikil)) ]
+    ,div [ Html.Attributes.style "font-size" "40pt" , Html.Attributes.style "color" "red"] [ text ( (String.fromInt model.marukei)++"/"++(String.fromInt (List.length data))++" "++(if model.seikai==Maru then "〇" else if model.seikai==Batu  then "✖" else "") )]
     ]
