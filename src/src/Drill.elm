e57ff92b8eba37189a39984971b943e388ef9c03
H Goto <gh6141@gmail.com>
2019-10-01 13:56:06 +0900

    initc

--- src/src/Drill.elm
+++ src/src/Drill.elm
@@ -245,12 +245,11 @@ view model =
     selectEvent = on "change" (Json.Decode.map Select targetValueMaybe )
 
     --dummy=["ion","shoka"]
-    textr raw=  Markdown.toHtmlWith {  defaultOptions | sanitize = False ,githubFlavored =Just {  tables =True , breaks =True
-              } }  [ ] raw
+    textr raw=  Markdown.toHtmlWith {  defaultOptions | sanitize = False  }  [ ] raw
     op dmy = List.map (\fname -> Html.option [value fname][text fname]) model.flist
-    bt numi xs =button [Html.Attributes.style "font-size" "20pt",Html.Attributes.style "height" "60pt",Html.Attributes.style "margin" "5pt",onClick (Answer numi) ] [ textr xs]
+    bt numi xs =button [Html.Attributes.style "background-color" "gray",Html.Attributes.style "font-size" "20pt",Html.Attributes.style "height" "60pt",Html.Attributes.style "margin" "5pt",onClick (Answer numi) ] [ textr xs]
     --bt numi xs =textr xs
-
+   --background-color:white;
   in
   
    div []
