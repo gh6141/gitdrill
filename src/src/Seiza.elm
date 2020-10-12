6fe257cf8389b150323a207be28c7deb32e10ea0
H Goto <gh6141@gmail.com>
2020-10-12 16:50:18 +0900

    up5

--- src/src/Seiza.elm
+++ src/src/Seiza.elm
@@ -7,6 +7,10 @@ import Html.Events exposing (..)
 
 import Task
 
+import Bootstrap.Button as Button
+import Bootstrap.CDN as CDN
+import Bootstrap.Utilities.Spacing as Spacing
+
 main =
   Browser.element
     { init = init
@@ -17,28 +21,56 @@ main =
 
 type alias Model =
   {
+    line:String
+    ,fname:String
   }
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( {
+  ( { line="_line" ,fname="n_star"
     }
   , Cmd.none
   )
 
 type Msg
-  = Msg
+  = Line | Noline | Kita | Striangle | Sasori
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  ( model
-  , Cmd.none
-  )
+ case msg of
+   Line ->
+    ( { model |  line="_line"   }
+    , Cmd.none
+    )
+   Noline ->
+    ( { model |   line=""      }
+    , Cmd.none)
+   Kita ->
+    ( {model | fname="n_star"}
+    , Cmd.none)
+   Striangle ->
+    ( {model | fname="s_triangle"}
+    , Cmd.none)
+   Sasori ->
+    ( {model | fname="sasori"}
+    , Cmd.none)
+  
 
 view : Model -> Html Msg
 view model =
   div []
     [
+      div [] [Button.button [Button.large ,Button.primary ,Button.attrs [Spacing.m1  ,onClick Kita]] [ text "北の空" ]
+      ,Button.button [Button.large ,Button.primary ,Button.attrs [Spacing.m1  ,onClick Striangle]] [ text "夏の大三角" ]
+      ,Button.button [Button.large ,Button.primary ,Button.attrs [Spacing.m1  ,onClick Sasori]] [ text "さそり座" ]
+      ]
+      ,
+      div []
+      [Button.button [Button.large ,Button.primary ,Button.attrs [Spacing.m1  ,onClick Line]] [ text "線でむすぶ" ]
+      ,Button.button [Button.large ,Button.primary ,Button.attrs [Spacing.m1  ,onClick Noline]] [ text "線なし" ]
+      ]
+      ,
+      img [src ("/py/" ++ model.fname ++ model.line ++ ".jpg")] []
     ]
 
 subscriptions : Model -> Sub Msg
