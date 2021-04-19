562f7abd9159dd926dfdaa8aaf85e07c005ecca0
H Goto <gh6141@gmail.com>
2021-04-19 13:39:05 +0900

    up

--- src/src/Nijo.elm
+++ src/src/Nijo.elm
@@ -40,12 +40,14 @@ type alias Model =
    ,cl :List Circle 
    ,plus: Bool
    ,kizami :Float
+   ,ichi :Int
+   ,sayu:Float
   }
 
 
 
 init : Model
-init = {cid=0,x="0",y="0",cl=[],plus=True,kizami=0.1}
+init = {cid=0,x="0",y="0",cl=[],plus=True,kizami=0.1,ichi=50,sayu=1.0}
 
 
 -- UPDATE
@@ -54,7 +56,7 @@ type Msg
   = Increment
   | Decrement
   | X2
-  | Mx2 | Kizami01 | Kizami001
+  | Mx2 | Kizami01 | Kizami001 | Kakudai | Shukusho |Genten
 
 
 update : Msg -> Model -> Model
@@ -69,13 +71,16 @@ update msg model =
       {model|cid=idx
       ,x=String.fromFloat ((toFloat idx)*kizami)
       ,y=String.fromFloat ((toFloat (idx^2))*kizami^2)
-      ,cl= {id=idx,xi=(toFloat idx)*kizami,yi=plusminus*((toFloat idx)*kizami)^2} :: model.cl}
+      ,cl= {id=idx,xi=(toFloat idx)*kizami,yi=plusminus*((toFloat idx)*kizami)^2} :: model.cl
+      ,sayu=1.0
+      }
 
     Decrement ->
       {model|cid=idx
       ,x=String.fromFloat ((toFloat idx)*kizami)
       ,y=String.fromFloat ((toFloat (idx^2))*kizami^2)
-      ,cl= {id=idx,xi=-(toFloat idx)*kizami,yi=plusminus*((toFloat idx)*kizami)^2} :: model.cl}
+      ,cl= {id=idx,xi=-(toFloat idx)*kizami,yi=plusminus*((toFloat idx)*kizami)^2} :: model.cl
+      ,sayu=-1.0}
 
     X2 ->
       {model|plus=True,cid=0,x="0",y="0",cl=[]}
@@ -86,6 +91,14 @@ update msg model =
        {model|cid=0,x="0",y="0",cl=[],kizami=0.1}
     Kizami001 ->
        {model|cid=0,x="0",y="0",cl=[],kizami=0.05}
+    Kakudai ->
+       {model|ichi=model.ichi+10}
+
+    Shukusho ->
+       {model|ichi=if model.ichi>10 then model.ichi-10 else 10}
+
+    Genten ->
+        {model|cid=0,x="0",y="0",cl=[]}
 
 
 -- VIEW
@@ -94,11 +107,9 @@ update msg model =
 view : Model -> Html Msg
 view model =
   let
- 
-
-
+   plusminus=if model.plus then 1.0 else -1.0
    fsize=10  --gusu
-   ichi=50 --1の値　倍率
+   ichi=model.ichi --1の値　倍率
    x0=200
    y0=200
    xjiku= 
@@ -114,19 +125,36 @@ view model =
    ichiy= mojidisp (x0+5) (y0-ichi+fsize//2) "1"
    ichimx= mojidisp (x0-ichi-fsize//2) (y0+15) "-1"
    ichimy= mojidisp (x0+5) (y0+ichi+fsize//2) "-1"
-   zahyojiku=[xjiku,yjiku,xmoji,ymoji,ichix,ichiy,ichimx,ichimy]
+
+   tofloat xs=Maybe.withDefault 0 (String.toFloat xs)
+   -- ++":   "++"x="++(String.left 4 model.x)++"  y="++(String.left 4 model.y) 
+   xichi=x0+(round (model.sayu*(toFloat ichi)*(tofloat model.x)))
+   yichi=y0-(round (plusminus*(toFloat ichi)*(tofloat model.y)))
+   xzahyo= mojidisp  (xichi-fsize//2) (y0+15) ((if model.sayu<0 then "-" else "")++(String.left 4 model.x))
+   yzahyo= mojidisp  (x0+5) (yichi+fsize//2) ((if model.plus then "" else "-")++(String.left 4 model.y))
+   xline= 
+    line [x1 (String.fromInt 0) ,y1 (String.fromInt yichi)
+        , x2 (String.fromInt (x0*2)), y2 (String.fromInt yichi),stroke "green"][]
+   yline= 
+    line [x1 (String.fromInt xichi) ,y1 (String.fromInt 0)
+        , x2 (String.fromInt xichi), y2 (String.fromInt (y0*2)),stroke "green"][]
+
+
+
+   zahyojiku=[xjiku,yjiku,xmoji,ymoji,xzahyo,yzahyo,xline,yline]
+
    
-   circleMsg ci = ciCreate (String.fromInt (x0+(round (ichi*ci.xi) )))  (String.fromInt (y0-(round (ichi*ci.yi))) ) 
+   circleMsg ci = ciCreate (String.fromInt (x0+(round ((toFloat ichi)*ci.xi) )))  (String.fromInt (y0-(round ((toFloat ichi)*ci.yi))) ) 
 
    pointDisp = List.map circleMsg model.cl  
   
   in
    div []
-    [ div [] [button [ onClick Increment ] [ text "プラス" ] ,button [ onClick Decrement ] [ text "マイナス" ]  ]
-    , div [] [ text ((if model.plus then "y=x^2" else "y=-x^2")++":   "++"x="++model.x++"  y="++model.y ) ]
+    [ div [] [button [ onClick Decrement ] [ text "マイナス" ] ,button [ onClick Genten ] [ text "原点" ] ,button [ onClick Increment ] [ text "プラス" ]   ]
+    , div [] [ text ((if model.plus then "y=x^2" else "y=-x^2")) ]
     , div [] [ button [onClick X2] [text "y=x^2"]  , button [onClick Mx2] [text "y=-x^2"]      ]
     , div [] [ button [onClick Kizami01] [text "きざみ０．１"]  , button [onClick Kizami001] [text "きざみ０．０５"]      ]
-
+    , div [] [ button [onClick Shukusho] [text "縮小"]  , button [onClick Kakudai] [text "拡大"]      ]
     , svg
     [ viewBox "0 0 800 600"
     , width "1000"
