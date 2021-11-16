d4a507728d38aed995f637f00f5aa8acdbc041fa
Hidemitsu Goto <gh6141@gmail.com>
2021-11-16 20:50:28 +0900

    up

--- src/src/Jukeizu.elm
+++ src/src/Jukeizu.elm
@@ -23,23 +23,16 @@ main =
 
 type alias Model =
   { 
-    dlist:List String
-    ,dlist1:List String
-    ,dlist2:List String
-    ,dlist3:List String
-    ,stage:String   
-    ,stage1:String
-    ,stage2:String
-    ,stage3:String
+    dlst:{fst:List String,snd:List String,thd:List String,fot:List String}
+    ,clist:{fst:String,snd:String,thd:String,fot:String}
   }
+
+type Junjo= Fst | Snd | Thd | Fot
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ({dlist=["?","あ","い","う","え"]
-    ,dlist1=["?","あ","い","う","え"]
-    ,dlist2=["?","あ","い","う","え"]
-    ,dlist3=["?","あ","い","う","え"]
-   ,stage="?",stage1="?",stage2="?",stage3="?"},Cmd.none)
+  ({dlst={fst=["?","あ","い","う","え"],snd=["?","あ","い","う","え"],thd=["?","あ","い","う","え"],fot=["?","あ","い","う","え"]}
+    ,clist={fst="?",snd="?",thd="?",fot="?"}},Cmd.none)
 
 type Msg
     = Change String |Change1 String |Change2 String |Change3 String
@@ -47,50 +40,60 @@ type Msg
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
    let
-      chikan lst src = List.map (\x-> (if x==src && src/="?" then ("*"++x) else x))  lst
+      chikan lst src = List.map (\x-> (if x==src then ("*"++x) else x))  lst
+
+       --?のときは、現在のmodel.listを参考に、復活させる
+      chikanx lst junjo clx=  List.map (\x-> 
+       
+       let 
+        cjj= case junjo of
+          Fst -> clx.fst
+          Snd -> clx.snd
+          Thd -> clx.thd
+          Fot -> clx.fot   
+
+       in      
+         if x== cjj  then cjj else x )  lst
+
+
+      dlcreate  clstx junjo stx= 
+       case stx of
+        "?" ->
+          case junjo of
+           Fst -> {fst=model.dlst.fst,snd=chikanx model.dlst.snd junjo clstx,thd=chikanx model.dlst.thd junjo clstx,fot=chikanx model.dlst.fot junjo clstx}
+           Snd -> {fst=chikanx model.dlst.fst junjo clstx,snd=model.dlst.snd ,thd=chikanx model.dlst.thd junjo clstx,fot=chikanx model.dlst.fot junjo clstx}
+           Thd -> {fst=chikanx model.dlst.fst junjo clstx,snd=chikanx model.dlst.snd junjo clstx,thd=model.dlst.thd ,fot=chikanx model.dlst.fot junjo clstx}
+           Fot -> {fst=chikanx model.dlst.fst junjo clstx,snd=chikanx model.dlst.snd junjo clstx,thd=chikanx model.dlst.thd junjo clstx,fot=model.dlst.fot}
+
+        _ ->
+         case junjo of
+           Fst -> {fst=model.dlst.fst,snd=chikan model.dlst.snd stx,thd=chikan model.dlst.thd stx,fot=chikan model.dlst.fot stx}
+           Snd -> {fst=chikan model.dlst.fst stx ,snd=model.dlst.snd ,thd=chikan model.dlst.thd stx,fot=chikan model.dlst.fot stx}
+           Thd -> {fst=chikan model.dlst.fst stx,snd=chikan model.dlst.snd stx,thd=model.dlst.thd ,fot=chikan model.dlst.fot stx}
+           Fot -> {fst=chikan model.dlst.fst stx,snd=chikan model.dlst.snd stx,thd=chikan model.dlst.thd stx,fot=model.dlst.fot}
 
    in
   case msg of
    Change st ->
-      let
-        dlt= model.dlist
-        dlt1=chikan model.dlist1 st
-        dlt2=chikan model.dlist2 st
-        dlt3=chikan model.dlist3 st
-        --st を消したdltが　できるが、もし、stが空白（実際は＊がついている）なら、復活させる
-
-      in 
-     ( { model |dlist=dlt,dlist1=dlt1,dlist2=dlt2,dlist3=dlt3,stage=st}
+    
+     ( { model |dlst= dlcreate  model.clist Fst st   ,clist={ fst= (if (String.left 1 st)=="*" then "?" else st)
+       ,snd=model.clist.snd,thd=model.clist.thd,fot=model.clist.fot}}
      , Cmd.none
      )
    Change1 st ->
-      let
-        dlt=chikan model.dlist st
-        dlt1= model.dlist1
-        dlt2=chikan model.dlist2 st
-        dlt3=chikan model.dlist3 st
-      in 
-     ( { model |dlist=dlt,dlist1=dlt1,dlist2=dlt2,dlist3=dlt3,stage1=st}
+      
+     ( { model |dlst= dlcreate  model.clist Snd st ,clist={fst=model.clist.fst  ,snd=(if (String.left 1 st)=="*" then "?" else st),
+      thd=model.clist.thd,fot=model.clist.fot}}
      , Cmd.none
      )
    Change2 st ->
-      let
-        dlt=chikan model.dlist st
-        dlt1=chikan model.dlist1 st
-        dlt2=model.dlist2
-        dlt3=chikan model.dlist3 st
-      in 
-     ( { model |dlist=dlt,dlist1=dlt1,dlist2=dlt2,dlist3=dlt3,stage2=st}
+      
+     ( { model |dlst= dlcreate  model.clist Thd  st ,clist={fst=model.clist.fst  ,snd=model.clist.snd   ,thd=(if (String.left 1 st)=="*" then "?" else st),fot=model.clist.fot }}
      , Cmd.none
      )
    Change3 st ->
-      let
-        dlt=chikan model.dlist st
-        dlt1=chikan model.dlist1 st
-        dlt2=chikan model.dlist2 st
-        dlt3=model.dlist3
-      in 
-     ( { model |dlist=dlt,dlist1=dlt1,dlist2=dlt2,dlist3=dlt3,stage3=st}
+  
+     ( { model |dlst= dlcreate  model.clist Fot st  ,clist={fst=model.clist.fst  ,snd=model.clist.snd,thd=model.clist.thd,fot=(if (String.left 1 st)=="*" then "?" else st)}}
      , Cmd.none
      )
   
@@ -115,16 +118,16 @@ view model =
     [
       div []
       [span [style "font-size" "20px"] [text ("どんな並べ方がある？")
-      ,select [style "font-size" "50px" ,onChange handler ] (List.map (\s -> Html.option [selected (s==model.stage),value s][text (if ((String.left 1 s)=="*") then "　" else s)]) (model.dlist))
-      ,select [style "font-size" "50px" ,onChange handler1 ] (List.map (\s -> Html.option [selected (s==model.stage1),value s][text (if ((String.left 1 s)=="*") then "　" else s)]) (model.dlist1))
-      ,select [style "font-size" "50px" ,onChange handler2 ] (List.map (\s -> Html.option [selected (s==model.stage2),value s][text (if ((String.left 1 s)=="*") then "　" else s)]) (model.dlist2))
-      ,select [style "font-size" "50px" ,onChange handler3 ] (List.map (\s -> Html.option [selected (s==model.stage3),value s][text (if ((String.left 1 s)=="*") then "　" else s)]) (model.dlist3))
+      ,select [style "font-size" "50px" ,onChange handler ] (List.map (\s -> Html.option [selected (s==model.clist.fst),value s][text (if ((String.left 1 s)=="*") then "　" else s)]) (model.dlst.fst))
+      ,select [style "font-size" "50px" ,onChange handler1 ] (List.map (\s -> Html.option [selected (s==model.clist.snd),value s][text (if ((String.left 1 s)=="*") then "　" else s)]) (model.dlst.snd))
+      ,select [style "font-size" "50px" ,onChange handler2 ] (List.map (\s -> Html.option [selected (s==model.clist.thd),value s][text (if ((String.left 1 s)=="*") then "　" else s)]) (model.dlst.thd))
+      ,select [style "font-size" "50px" ,onChange handler3 ] (List.map (\s -> Html.option [selected (s==model.clist.fot),value s][text (if ((String.left 1 s)=="*") then "　" else s)]) (model.dlst.fot))
       ]
        
       ]
       ,
       div []
-      [text (model.stage++model.stage1++model.stage2++model.stage3)]
+      [text (model.clist.fst++model.clist.snd++model.clist.thd++model.clist.fot)]
     ]
     
 
