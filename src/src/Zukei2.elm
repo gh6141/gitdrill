60941046c4dd825d7cbddca39f20244622a5ebda
gh <gh6141@gmail.com>
2023-06-03 13:48:01 +0900

    up

--- src/src/Zukei2.elm
+++ src/src/Zukei2.elm
@@ -13,6 +13,13 @@ import Svg.Attributes as Attr
 import Svg.Events exposing (onMouseUp)
 import Svg.Keyed
 import Svg.Lazy exposing (lazy)
+import Html exposing (Attribute)
+import Random
+
+
+
+import Svg exposing (svg, polygon)
+import Svg.Attributes exposing (fill, points)
 
 
 main : Program () Model Msg
@@ -40,9 +47,17 @@ type alias Id =
     String
 
 
-makeBox : Id -> Vec2 -> Color -> Zukei ->Box
-makeBox id position col zu=
-    Box id position col zu
+--makeBox : Id -> Vec2 -> Color -> Zukei ->Box
+--makeBox id position col zu=
+ --   Box id position col zu
+
+makeBox : Id ->Zokusei->Box
+makeBox id zoku=
+    Box id zoku.position zoku.color zoku.zukei
+
+
+
+
 
 
 dragBoxBy : Vec2 -> Box -> Box
@@ -65,18 +80,25 @@ emptyGroup =
     BoxGroup 0 Nothing []
 
 
-addBox : Color -> Zukei ->Vec2 ->  BoxGroup -> BoxGroup
-addBox col zuk position  ({ uid, idleBoxes } as group) =
-    { group
-        | idleBoxes = makeBox (String.fromInt uid) position col zuk :: idleBoxes
-        , uid = uid + 1
-    }
+--addBox : Color -> Zukei ->Vec2 ->  BoxGroup -> BoxGroup
+--addBox col zuk position  ({ uid, idleBoxes } as group) =
+  --  { group
+  --      | idleBoxes = makeBox (String.fromInt uid) position col zuk :: idleBoxes
+  --      , uid = uid + 1
+  --  }
+
+--addBox : Zokusei ->  BoxGroup -> BoxGroup
+addBox zoku ({uid,idleBoxes} as group) = {group| idleBoxes =  makeBox (String.fromInt uid) zoku :: idleBoxes , uid = uid + 1 }
 
 
-makeBoxGroup : List Vec2 -> BoxGroup
-makeBoxGroup positions =
-    positions
-        |> List.foldl (addBox Green Triangle) emptyGroup
+--makeBoxGroup : List Vec2 -> BoxGroup
+--makeBoxGroup positions =
+--    positions
+--        |> List.foldl (addBox Green Triangle) emptyGroup
+
+makeBoxGroup attributes =
+    attributes
+       |> List.foldl addBox emptyGroup
 
 
 allBoxes : BoxGroup -> List Box
@@ -126,18 +148,57 @@ type Msg
     | StopDragging
 
 
+fixedYCoordinate y = \x -> Vector2.vec2 x y
+
 boxPositions : List Vec2
 boxPositions =
     let
         indexToPosition =
-            toFloat >> (*) 60 >> (+) 10 >> Vector2.vec2 10
+          --  toFloat >> (*) 60 >> (+) 10 >> Vector2.vec2 10
+          toFloat >> (*) 60 >> (+) 90 >> fixedYCoordinate 330
     in
     List.range 0 10 |> List.map indexToPosition
+
+type alias Zokusei = {position:Vec2,color:Color,zukei:Zukei}
+
+boxAttributes : List Zokusei
+boxAttributes  =
+    let
+        indexToAttribute =
+          --  toFloat >> (*) 60 >> (+) 10 >> Vector2.vec2 10
+          toFloat >> (*) 60 >> (+) 90 >> fixedYCoordinateAndAttribute 330
+    in
+    List.range 0 10 |> List.map indexToAttribute
+
+rcol: Int -> Color
+rcol rn =   case rn of
+       0 -> Red
+       1 -> Green
+       2 -> Yellow
+       _ -> Yellow
+
+rzukei rn = case rn of
+       0 -> Circle
+       1 -> Triangle
+       2 -> Cube
+       _ -> Cube
+
+fixedYCoordinateAndAttribute y =
+   (\x -> {position=(Vector2.vec2 x y),color=rcol (modBy 2 (((x-90)/60)|>floor) ),zukei=rzukei (modBy 3 (((x-90)/60)|>floor) )} )
+
+
+--  (randomNumber, newSeed) = generateRandomInt 0 10 Random.initialSeed
+
+
+       
+
 
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    ( { boxGroup = makeBoxGroup boxPositions
+    ( {         
+        --boxGroup = makeBoxGroup boxPositions
+        boxGroup = makeBoxGroup boxAttributes
       , drag = Draggable.init
       }
     , Cmd.none
@@ -198,6 +259,9 @@ view { boxGroup } =
             [ background
             ,(boxwaku "100" "100"),(boxwaku "320" "100"),boxwaku "540" "100"
             , boxesView boxGroup
+            , boxView {id="0",position=Vector2.vec2 120 110  ,color=Yellow,zukei=Triangle}
+             , boxView {id="0",position=Vector2.vec2 330 110  ,color=Yellow,zukei=Circle}
+             , boxView {id="0",position=Vector2.vec2 550 110  ,color=Yellow,zukei=Cube}
      
             ]
         ]
@@ -222,6 +286,7 @@ boxView { id, position,color,zukei } =
               Yellow -> "yellow"
 
 
+
     hyoji zuk=case zuk of
      Cube ->
       Svg.rect
@@ -236,25 +301,38 @@ boxView { id, position,color,zukei } =
         , onMouseUp StopDragging
         ]++ (Draggable.touchTriggers id DragMsg) )
         []
+
+
+
+
      Triangle ->
-       Svg.rect
-       ( [ num Attr.width <| getX boxSize
-        , num Attr.height <| getY boxSize
-        , num Attr.x (getX position)
-        , num Attr.y (getY position)
-        , Attr.fill (colorx color)
-        , Attr.stroke "yellow"
+      let
+         bai=0.7
+         dx=21
+         dy=32
+         topPoint = (String.fromFloat ((getX position)+0+dx))++","++(String.fromFloat ((getY position)-50*bai+dy))
+         --leftPoint = "-43.3, 25"
+         leftPoint = (String.fromFloat ((getX position)-43.3*bai+dx))++","++(String.fromFloat ((getY position)+25*bai+dy))
+         --rightPoint = "43.3, 25"
+         rightPoint = (String.fromFloat ((getX position)+43.3*bai+dx))++","++(String.fromFloat ((getY position)+25*bai+dy))
+
+         posS=topPoint++" "++leftPoint++" "++rightPoint
+      in
+         Svg.polygon ([ 
+         Attr.stroke "black"
         , Attr.cursor "move"
         , Draggable.mouseTrigger id DragMsg
-        , onMouseUp StopDragging
-        ]++ (Draggable.touchTriggers id DragMsg) )
-        []
+        , onMouseUp StopDragging            
+        ,fill  (colorx color), points  posS  ]++ (Draggable.touchTriggers id DragMsg)) []
+  
+
+
      Circle ->
-            Svg.rect
-       ( [ num Attr.width <| getX boxSize
-        , num Attr.height <| getY boxSize
-        , num Attr.x (getX position)
-        , num Attr.y (getY position)
+            Svg.circle
+       ( [
+         numh Attr.r <| getY boxSize
+        , num Attr.cx ((getX position)+25.0)
+        , num Attr.cy ((getY position)+25.0)
         , Attr.fill (colorx color)
         , Attr.stroke "black"
         , Attr.cursor "move"
@@ -293,5 +371,13 @@ boxwaku xs ys=
 
 
 num : (String -> Svg.Attribute msg) -> Float -> Svg.Attribute msg
+
+
+
 num attr value =
+
     attr (String.fromFloat value)
+
+numh attr value =
+
+    attr (String.fromFloat (value/2))
